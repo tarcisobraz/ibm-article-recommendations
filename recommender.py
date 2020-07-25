1e26959b9cb08f5dcdafa371b183456d528f24c5
Tarciso Braz <tarcisocomp@gmail.com>
2020-07-25 03:20:42 +0000

    feat: Adds code to save recommendations for all users using different recommendation algorithms.

--- recommender.py
+++ recommender.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import pickle
 import sys # can use sys to take command line arguments
 
 from recommender_functions import *
@@ -36,7 +37,7 @@ class Recommender():
         svd_latent_factors_mat_ = None
         user_articles_interactions_ = None
 
-    def fit(self):
+    def fit(self, verbose=False):
         '''
         Builds all intermediate datasets to be used for prediction / recommendation
         using movies and reviews data.
@@ -48,37 +49,38 @@ class Recommender():
         Parameters
         ----------
         self : object
+        verbose : boolean, whether the output should be verbose (print logs)
         
         Returns
         -------
         self : object
             Returns self.
         '''
-        print('articles_df.shape:', self.articles_df.shape)
-        print('interactions_df.shape:', self.interactions_df.shape)
+        if verbose: print('articles_df.shape:', self.articles_df.shape)
+        if verbose: print('interactions_df.shape:', self.interactions_df.shape)
         print('Setting article id to string in both input dfs...')
         self.articles_df.loc[:,'article_id'] = self.articles_df.loc[:,'article_id'].astype(str)
         self.interactions_df.loc[:,'article_id'] = self.interactions_df.loc[:,'article_id'].astype(str)
-        print('articles_df types:', self.articles_df.dtypes)
-        print('interactions_df types:', self.interactions_df.dtypes)
+        if verbose: print('articles_df types:', self.articles_df.dtypes)
+        if verbose: print('interactions_df types:', self.interactions_df.dtypes)
         print('Building Full Articles Dataset...')
         self.all_articles = build_all_articles_dataset(self.articles_df, self.interactions_df)
         print('Building Ranked Articles DF...')
         self.ranked_articles_ = get_top_article_ids(self.interactions_df.shape[0], self.interactions_df)
-        print(self.ranked_articles_[:10], len(self.ranked_articles_))
+        if verbose: print(self.ranked_articles_[:10], len(self.ranked_articles_))
         print('Building User-Article Interactions Matrix...')
         self.user_article_matrix_ = user_item = create_user_item_matrix(self.interactions_df)
-        print(self.user_article_matrix_.head(10), self.user_article_matrix_.shape)
+        if verbose: print(self.user_article_matrix_.head(10), self.user_article_matrix_.shape)
         print('Building Articles Similarity Matrix...')
         self.articles_sim_matrix_ = build_articles_similarity_matrix(self.all_articles)
-        print(self.articles_sim_matrix_.shape)
-        print(self.articles_sim_matrix_)
+        if verbose: print(self.articles_sim_matrix_.shape)
+        if verbose: print(self.articles_sim_matrix_)
         print('Building User Articles Interactions DataFrame...')
         self.user_articles_interactions_ = build_user_articles_interactions(self.interactions_df)
-        print(self.user_articles_interactions_.head())
+        if verbose: print(self.user_articles_interactions_.head())
         print('Building Users and Movies SVD Matrices...')
         self.svd_user_mat_, self.svd_latent_factors_mat_, self.svd_article_mat_ = np.linalg.svd(self.user_article_matrix_)
-        print('S shape:',self.svd_latent_factors_mat_.shape, 
+        if verbose: print('S shape:',self.svd_latent_factors_mat_.shape, 
               'U shape:',self.svd_user_mat_.shape, 
               'VT shape:',self.svd_article_mat_.shape)
         
@@ -153,8 +155,8 @@ class Recommender():
         recs_names = []
 
         if entity_type == 'article':
-            if verbose: print('Article Recommendation for article ID:', entity_id)
-            if verbose: print('Using Content-Based Recommendation...')
+            print('Article Recommendation for article ID:', entity_id)
+            print('Using Content-Based Recommendation...')
             recs_ids = find_similar_articles(article_id, 
                                              self.all_articles, 
                                              self.articles_sim_matrix_, 
@@ -162,10 +164,10 @@ class Recommender():
                                              verbose=verbose)
 
         elif entity_type == 'user':
-            if verbose: print('Article Recommendation for user ID:', entity_id)
+            print('Article Recommendation for user ID:', entity_id)
             if rec_type == 1:
                 #Collaborative Filtering
-                if verbose: print('Using Collaborative Filtering Recommendation...')
+                print('Using Collaborative Filtering Recommendation...')
                 recs_ids = user_user_recs_part2(entity_id, 
                                                 self.user_article_matrix_, 
                                                 self.interactions_df, 
@@ -173,7 +175,7 @@ class Recommender():
                                                 verbose=verbose)
             elif rec_type == 2:
                 #Content-Based
-                if verbose: print('Using Content-Based Recommendation...')
+                print('Using Content-Based Recommendation...')
                 recs_ids = make_content_recs2(entity_id, 
                                               self.all_articles, 
                                               self.user_articles_interactions_, 
@@ -183,7 +185,7 @@ class Recommender():
                 
             elif rec_type == 3:
                 #Matrix Factorization (SVD)
-                if verbose: print('Using Matrix Factorization (SVD) Recommendation...')
+                print('Using Matrix Factorization (SVD) Recommendation...')
                 recs_ids = make_svd_recs(entity_id, self.user_article_matrix_, 
                                          self.svd_user_mat_, self.svd_article_mat_,
                                          self.svd_latent_factors_mat_, num_latent_features=10, 
@@ -218,61 +220,82 @@ if __name__ == '__main__':
     rec.fit()
     
     #Testing SVD prediction evaluation function
-    df_train = interactions.head(40000).copy()
-    df_test = interactions.tail(5993).copy()
-    rec.predict_evaluate(df_train, df_test, 10, True)
+#     df_train = interactions.head(40000).copy()
+#     df_test = interactions.tail(5993).copy()
+#     rec.predict_evaluate(df_train, df_test, 10, True)
     
     #Testing SVD prediction for a user-article pair
     #rec.predict_interaction(user_id=8, article_id='1429.0', verbose=True)
     
     #Testing Interaction prediction for users-articles within the interactions dataset
-    for i in range(20):
-        user_id = interactions.iloc[i,:].user_id
-        article_id = interactions.iloc[i,:].article_id
-        print('Interaction Prediction between User {} and Article {}: {}'.format(
-            user_id,article_id,rec.predict_interaction(user_id,article_id)))
+#     for i in range(20):
+#         user_id = interactions.iloc[i,:].user_id
+#         article_id = interactions.iloc[i,:].article_id
+#         print('Interaction Prediction between User {} and Article {}: {}'.format(
+#             user_id,article_id,rec.predict_interaction(user_id,article_id)))
     
     #Testing Collaborative-Filtering Article recommendation for users within the dataset
-    recs_dict = {}
-    for i in range(5):
-        print()
-        print()
-        print('================')
-        user_id = interactions.iloc[i,:].user_id
-        print('Making Recommendations for User ID:', user_id)
-        recs_dict[user_id] = rec.make_recs(user_id, 'user', verbose=True)
+#     recs_dict = {}
+#     for i in range(5):
+#         print()
+#         print()
+#         print('================')
+#         user_id = interactions.iloc[i,:].user_id
+#         print('Making Recommendations for User ID:', user_id)
+#         recs_dict[user_id] = rec.make_recs(user_id, 'user', verbose=True)
         
     #Testing Content-Based Article recommendation for users within the dataset
-    recs_dict = {}
-    for i in range(5):
-        print()
-        print()
-        print('================')
-        user_id = interactions.iloc[i,:].user_id
-        print('Making Recommendations for User ID:', user_id)
-        recs_dict[user_id] = rec.make_recs(user_id, 'user', rec_type=2, verbose=True)
+#     recs_dict = {}
+#     for i in range(5):
+#         print()
+#         print()
+#         print('================')
+#         user_id = interactions.iloc[i,:].user_id
+#         print('Making Recommendations for User ID:', user_id)
+#         recs_dict[user_id] = rec.make_recs(user_id, 'user', rec_type=2, verbose=True)
         
     #Testing Article recommendation for articles within the dataset
-    recs_dict = {}
-    for i in range(5):
-        print()
-        print()
-        print('================')
-        article_id = interactions.iloc[i,:].article_id
-        print('Making Recommendations for Article ID:', article_id)
-        recs_dict[article_id] = rec.make_recs(article_id, 'article', verbose=True)
+#     recs_dict = {}
+#     for i in range(5):
+#         print()
+#         print()
+#         print('================')
+#         article_id = interactions.iloc[i,:].article_id
+#         print('Making Recommendations for Article ID:', article_id)
+#         recs_dict[article_id] = rec.make_recs(article_id, 'article', verbose=True)
 
     #Testing SVD Article recommendation for users within the dataset
-    recs_dict = {}
-    for i in range(5):
-        print()
-        print()
-        print('================')
-        user_id = interactions.iloc[i,:].user_id
-        print('Making Recommendations for User ID:', user_id)
-        recs_dict[user_id] = rec.make_recs(user_id, 'user', rec_type=3, verbose=True)
+#     recs_dict = {}
+#     for i in range(5):
+#         print()
+#         print()
+#         print('================')
+#         user_id = interactions.iloc[i,:].user_id
+#         print('Making Recommendations for User ID:', user_id)
+#         recs_dict[user_id] = rec.make_recs(user_id, 'user', rec_type=3, verbose=True)
         
 
     #Testing Movie recommendation for users outside the dataset
     #print(rec.make_recs(40000, 'user'))
     
+    #Saving all user recommendations using User-User Collaborative Filtering Recommendation
+#     unique_users = pd.unique(interactions.user_id)
+#     collab_filtering_users_recs = {}
+#     for i in range(unique_users.shape[0]):
+#         user_id = unique_users[i]
+#         print('Making Recommendations for User ID:', user_id)
+#         recs_dict[user_id] = rec.make_recs(user_id, 'user')
+    
+#     with open('data/collab_filtering_users_recs.pickle', 'wb') as handle:
+#         pickle.dump(collab_filtering_users_recs, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        
+    #Saving all user recommendations using Content-Based Recommendation
+    unique_users = pd.unique(interactions.user_id)
+    content_based_users_recs = {}
+    for i in range(unique_users.shape[0]):
+        user_id = unique_users[i]
+        print('Making Recommendations for User ID:', user_id)
+        recs_dict[user_id] = rec.make_recs(user_id, 'user', rec_type=2)
+    
+    with open('data/content_based_users_recs.pickle', 'wb') as handle:
+        pickle.dump(content_based_users_recs, handle, protocol=pickle.HIGHEST_PROTOCOL)
